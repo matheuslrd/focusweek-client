980b614cc7556e985d83a73510d3901f00930128
Matheus Laurindo <matheus104521@gmail.com>
2024-09-15 20:41:52 -0300

    feat: summary page

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,27 @@
-import { Plus } from 'lucide-react';
-import letsStartIllustration from './assets/lets-start.svg';
-import logo from './assets/logo-in-orbit.svg';
-import { Button } from './components/ui/button';
+import { CreateGoalModal } from './components/create-goal';
+import { Summary } from './components/summary';
+import { Dialog } from './components/ui/dialog';
 
 export function App() {
     return (
-        <div className="h-screen flex flex-col items-center justify-center gap-8">
-            <img src={logo} alt="logo in orbit" />
-            <img src={letsStartIllustration} alt="lets start illustration" />
-            <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-                Você ainda não cadastrou nenhuma meta, que tal começar agora mesmo?
-            </p>
+        <Dialog>
+            {/* <div className="h-screen flex flex-col items-center justify-center gap-8">
+                <img src={logo} alt="logo in orbit" />
+                <img src={letsStartIllustration} alt="lets start illustration" />
+                <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+                    Você ainda não cadastrou nenhuma meta, que tal começar agora mesmo?
+                </p>
 
-            <Button>
-                <Plus className="size-4" /> Cadastrar meta
-            </Button>
-        </div>
+                <DialogTrigger asChild>
+                    <Button>
+                        <Plus className="size-4" /> Cadastrar meta
+                    </Button>
+                </DialogTrigger>
+            </div> */}
+
+            <Summary />
+
+            <CreateGoalModal />
+        </Dialog>
     );
 }
