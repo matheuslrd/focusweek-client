931a74b65b6cb44c1f11ef8196abc832f4c78802
Matheus Laurindo <matheus104521@gmail.com>
2024-09-15 20:42:55 -0300

    Revert "feat: summary page"
    This reverts commit 980b614cc7556e985d83a73510d3901f00930128.

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,20 @@
-import { CreateGoalModal } from './components/create-goal';
-import { Summary } from './components/summary';
-import { Dialog } from './components/ui/dialog';
+import { Plus } from 'lucide-react';
+import letsStartIllustration from './assets/lets-start.svg';
+import logo from './assets/logo-in-orbit.svg';
+import { Button } from './components/ui/button';
 
 export function App() {
     return (
-        <Dialog>
-            {/* <div className="h-screen flex flex-col items-center justify-center gap-8">
-                <img src={logo} alt="logo in orbit" />
-                <img src={letsStartIllustration} alt="lets start illustration" />
-                <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-                    Você ainda não cadastrou nenhuma meta, que tal começar agora mesmo?
-                </p>
+        <div className="h-screen flex flex-col items-center justify-center gap-8">
+            <img src={logo} alt="logo in orbit" />
+            <img src={letsStartIllustration} alt="lets start illustration" />
+            <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+                Você ainda não cadastrou nenhuma meta, que tal começar agora mesmo?
+            </p>
 
-                <DialogTrigger asChild>
-                    <Button>
-                        <Plus className="size-4" /> Cadastrar meta
-                    </Button>
-                </DialogTrigger>
-            </div> */}
-
-            <Summary />
-
-            <CreateGoalModal />
-        </Dialog>
+            <Button>
+                <Plus className="size-4" /> Cadastrar meta
+            </Button>
+        </div>
     );
 }
